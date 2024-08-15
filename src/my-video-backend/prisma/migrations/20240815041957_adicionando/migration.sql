4e246fb86ffcb4333c9ef9849b8bde246daa9905
Gabriel <gabriel10919@outlook.com>
2024-08-15 01:38:32 -0300

    Nova mensagem de commitAdicionando POST de cadastro de usuário
    Co-authored-by: marrcelo <magalhaesmarcelo@outlook.com.br>

---
+++ src/my-video-backend/prisma/migrations/20240815041957_adicionando/migration.sql
@@ -0,0 +1,14 @@
+/*
+  Warnings:
+
+  - Added the required column `updatedAt` to the `Midia` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updatedAt` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Midia" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
